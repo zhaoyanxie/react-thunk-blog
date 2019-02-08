73bf5d26c23f00e8f35f680f9f114e4e4639619c
xie-zhaoyan <xiezhaoyan@amaris.ai>
2019-02-08 09:59:00 +0800

    Wire up action creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { fetchPosts } from "../actions";
 
 class PostList extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>PostList</div>;
   }
 }
 
-export default PostList;
+export default connect(
+  null,
+  { fetchPosts: fetchPosts }
+)(PostList);
