cc7ad550268fa75a240ae67d04274a801a606344
xie-zhaoyan <xiezhaoyan@amaris.ai>
2019-02-19 13:58:18 +0800

    Connect PostList component to access posts as props

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  posts: postsReducer
+  posts: postsReducer // key of this object will be the key in the app's state
 });
