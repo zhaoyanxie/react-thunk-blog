72a91f8a7f15a0b580cf0d16ad5dd4212b3b8422
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2019-02-19 22:29:23 +0800

    action creator to find unique user

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { fetchUser } from "../actions";
-
 class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
+    console.log(this.props);
     const { user } = this.props;
     if (!user) return null;
     return <div className="header">{user.name}</div>;
@@ -19,7 +14,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) }; // ownProps.userId is the this.props.userId in the component
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchUser }
-)(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
